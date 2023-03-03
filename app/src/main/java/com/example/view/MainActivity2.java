38a3a99333c50d997940e732e581ac5dfb6c4d56
yn030727 <2601098388@qq.com>
2023-03-03 15:13:28 +0800

    1. finish view

--- app/src/main/java/com/example/view/MainActivity2.java
+++ app/src/main/java/com/example/view/MainActivity2.java
@@ -4,14 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    View view ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        View view = new View(this);
+        view = new View(this);
 //        view.measure();
+        ViewGroup viewGroup = new ViewGroup(this) {
+            @Override
+            protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+            }
+        };
+
+
+        LinearLayout linearLayout = new LinearLayout(this);
+
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if(hasFocus){
+            int width = view.getMeasuredWidth();
+            int height = view.getMeasuredHeight();
+        }
     }
 }
