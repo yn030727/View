6879979c18cf4190278accb8c23d17f9a16ecce1
yn030727 <2601098388@qq.com>
2023-03-03 14:29:36 +0800

    1. finish view

---
+++ app/src/main/java/com/example/view/MainActivity2.java
@@ -0,0 +1,17 @@
+package com.example.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        View view = new View(this);
+//        view.measure();
+    }
+}
