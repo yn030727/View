1ee7c5d1c607b140cd6b32eb210a493af83c4366
yn030727 <2601098388@qq.com>
2023-03-06 14:54:28 +0800

    1. finish view

--- app/src/main/java/com/example/view/ui/CircleView.java
+++ app/src/main/java/com/example/view/ui/CircleView.java
@@ -16,18 +16,22 @@ public class CircleView extends View {
 
     public CircleView(Context context) {
         super(context);
+        init();
     }
 
     public CircleView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public CircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
 
     public CircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        init();
     }
 
     //方法
@@ -41,9 +45,36 @@ public class CircleView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        int width = getWidth();
-        int height = getHeight();
+        //处理padding问题
+        final int paddingLeft = getPaddingLeft();
+        final int paddingRight = getPaddingRight();
+        final int paddingTop = getPaddingTop();
+        final int paddingBottom = getPaddingBottom();
+
+        //padding是内部到当前View边框的距离
+        //width是当前View的宽度
+        //所以减去之后的宽高就是绘制出来的结果
+        int width = getWidth() - paddingLeft - paddingRight;
+        int height = getHeight() - paddingTop - paddingBottom;
         int radius = Math.min(width , height) / 2;
-        canvas.drawCircle(width / 2 , height / 2 , radius , mPaint);
+        //绘制的过程
+        canvas.drawCircle(paddingLeft + width / 2 , paddingTop + height / 2 , radius , mPaint);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        //对于继承View的自定义View需要处理wrap_content
+        int widthSpecMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSpecSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightSpecMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSpecSize = MeasureSpec.getSize(heightMeasureSpec);
+        if(widthSpecMode == MeasureSpec.AT_MOST && heightSpecMode == MeasureSpec.AT_MOST){
+            setMeasuredDimension(200 , 200);
+        }else if(widthSpecMode == MeasureSpec.AT_MOST){
+            setMeasuredDimension(200 , heightSpecSize);
+        }else if(heightSpecMode == MeasureSpec.AT_MOST){
+            setMeasuredDimension(widthSpecSize,200);
+        }
     }
 }
