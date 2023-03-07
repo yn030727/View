76582b0b1c8e79faf33833a305592bf0e230a826
yn030727 <2601098388@qq.com>
2023-03-07 21:42:57 +0800

    1. finish view

--- drawable/src/main/java/com/example/drawable/MainActivity.java
+++ drawable/src/main/java/com/example/drawable/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.drawable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     @SuppressLint("MissingInflatedId")
@@ -12,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText editText = (EditText) findViewById(R.id.text1);
+//        EditText editText = (EditText) findViewById(R.id.text1);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        TransitionDrawable drawable = (TransitionDrawable) imageView.getDrawable();
+        drawable.startTransition(1000);
     }
 }
