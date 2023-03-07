14bd7c389583d644f32f78a1cacd49ee1c816fa7
yn030727 <2601098388@qq.com>
2023-03-07 20:40:14 +0800

    1. finish view

--- drawable/src/main/java/com/example/drawable/MainActivity.java
+++ drawable/src/main/java/com/example/drawable/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.drawable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText editText = (EditText) findViewById(R.id.text1);
     }
 }
