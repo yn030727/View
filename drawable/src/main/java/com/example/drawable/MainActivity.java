19260623687c8d57e57aed36e26b23f57b836ed5
yn030727 <2601098388@qq.com>
2023-03-08 14:59:44 +0800

    1. finish view

--- drawable/src/main/java/com/example/drawable/MainActivity.java
+++ drawable/src/main/java/com/example/drawable/MainActivity.java
@@ -18,19 +18,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        EditText editText = (EditText) findViewById(R.id.text1);
-        ImageView imageView = (ImageView) findViewById(R.id.imageView);
-        //如果是View，就是getBackground
-        TransitionDrawable drawable = (TransitionDrawable) imageView.getDrawable();
-        drawable.startTransition(1000);
+////        EditText editText = (EditText) findViewById(R.id.text1);
+//        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+//        //如果是View，就是getBackground
+//        TransitionDrawable drawable = (TransitionDrawable) imageView.getDrawable();
+//        drawable.startTransition(1000);
+//
+//        ImageView imageView1 = (ImageView) findViewById(R.id.imageView2);
+//        ScaleDrawable scaleDrawable = (ScaleDrawable) imageView1.getDrawable();
+//        scaleDrawable.setLevel(1);
+//
+//        //剪切
+//        ImageView testClip = (ImageView) findViewById(R.id.imageView3);
+//        ClipDrawable clipDrawable = (ClipDrawable) testClip.getDrawable();
+//        clipDrawable.setLevel(5000);
 
-        ImageView imageView1 = (ImageView) findViewById(R.id.imageView2);
-        ScaleDrawable scaleDrawable = (ScaleDrawable) imageView1.getDrawable();
-        scaleDrawable.setLevel(1);
 
-        //剪切
-        ImageView testClip = (ImageView) findViewById(R.id.imageView3);
-        ClipDrawable clipDrawable = (ClipDrawable) testClip.getDrawable();
-        clipDrawable.setLevel(5000);
     }
 }
