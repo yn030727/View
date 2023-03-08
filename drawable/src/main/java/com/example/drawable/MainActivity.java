0a644eaf5d3a9985bcc501e2280920be148d3373
yn030727 <2601098388@qq.com>
2023-03-08 16:13:54 +0800

    1. finish view

--- drawable/src/main/java/com/example/drawable/MainActivity.java
+++ drawable/src/main/java/com/example/drawable/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.drawable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.ClipDrawable;
 import android.graphics.drawable.ScaleDrawable;
 import android.graphics.drawable.TransitionDrawable;
@@ -14,11 +15,15 @@ import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +44,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //        ClipDrawable clipDrawable = (ClipDrawable) testClip.getDrawable();
 //        clipDrawable.setLevel(5000);
 //        Log.d("Ning","1");
-       Button button = (Button) findViewById(R.id.button);
+//        Button button = (Button) findViewById(R.id.button);
 //        Animation animation = AnimationUtils.loadAnimation(this,R.anim.filename);
 //        button.startAnimation(animation);
 //        button.setOnClickListener(this);
@@ -47,34 +52,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         //代码设置动画
         //创建了一个透明度动画，在300ms内将一个Button从0变为1
-        AlphaAnimation alphaAnimation = new AlphaAnimation(0,1);
-        alphaAnimation.setDuration(3000);
-        button.startAnimation(alphaAnimation);
-        alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
-            @Override
-            public void onAnimationStart(Animation animation) {
+//        AlphaAnimation alphaAnimation = new AlphaAnimation(0,1);
+//        alphaAnimation.setDuration(3000);
+//        button.startAnimation(alphaAnimation);
+//        alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
+//            @Override
+//            public void onAnimationStart(Animation animation) {
+//
+//            }
+//
+//            @Override
+//            public void onAnimationEnd(Animation animation) {
+//
+//            }
+//
+//            @Override
+//            public void onAnimationRepeat(Animation animation) {
+//
+//            }
+//        });
 
-            }
+//        Button mButton = (Button) findViewById(R.id.button);
+//        mButton.setBackgroundResource(R.drawable.animation_drawable);
+//        AnimationDrawable drawable = (AnimationDrawable) mButton.getBackground();
+//        drawable.start();
 
-            @Override
-            public void onAnimationEnd(Animation animation) {
-
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-
-            }
-        });
-
+        ListView listView = (ListView) findViewById(R.id.list1);
+        String[] data = {"apply","banana","sfaf","sfafaw","fawerwe","24","fwefa","fawfawf"};
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(MainActivity.this , android.R.layout.simple_list_item_1,data);
+        listView.setAdapter(arrayAdapter);
+//        Animation animation = AnimationUtils.loadAnimation(this , R.anim.anim_item);
+//        LayoutAnimationController controller = new LayoutAnimationController(animation);
+//        controller.setDelay(0.5f);
+//        controller.setOrder(LayoutAnimationController.ORDER_NORMAL);
+//        listView.setLayoutAnimation(controller);
 
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.button:
-                Log.d("Ning","click");
+//            case R.id.button:
+//                Log.d("Ning","click");
         }
     }
 }
