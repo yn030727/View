560e883a7eaa3f6c52f77e3e9cccd75ac820681c
yn030727 <2601098388@qq.com>
2023-03-20 11:28:04 +0800

    1. finish view

--- app/handler1/src/main/java/com/example/handler1/MainActivity.java
+++ app/handler1/src/main/java/com/example/handler1/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.handler1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ThreadLocal<Boolean> mBooleanThreadLocal = new ThreadLocal<Boolean>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String TAG = "Ning";
+
+
+        mBooleanThreadLocal.set(true);
+        Log.d(TAG, "[Thread#main]mBooleanThreadLocal = " + mBooleanThreadLocal.get());
+
+
+        new Thread("Thread#1"){
+            @Override
+            public void run() {
+                mBooleanThreadLocal.set(false);
+                Log.d(TAG, "[Thread#1]mBooleanThreadLocal = " + mBooleanThreadLocal.get());
+            }
+        }.start();
+
+        new Thread("Thread#2"){
+            @Override
+            public void run() {
+                Log.d(TAG, "[Thread#2]mBooleanThreadLocal = " + mBooleanThreadLocal.get());
+            }
+        }.start();
+
     }
 }
