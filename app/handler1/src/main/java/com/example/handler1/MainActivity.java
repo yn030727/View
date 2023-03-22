e6f610c3a3fc063bdc06228d102a4d2b6f575875
yn030727 <2601098388@qq.com>
2023-03-22 18:33:36 +0800

    1. finish view

--- app/handler1/src/main/java/com/example/handler1/MainActivity.java
+++ app/handler1/src/main/java/com/example/handler1/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
@@ -56,5 +59,33 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        }.start();
 
+    }
+
+
+    public static Bitmap decodeSampledBitmapFromResource(Resources res , int resId , int reqWidth , int reqHeight){
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res , resId , options);
+
+        //计算inSampleSize
+        options.inSampleSize = calculateInSampleSize(options , reqWidth , reqHeight);
+
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res , resId , options);
+    }
+    public static int calculateInSampleSize(BitmapFactory.Options options , int reqWidth , int reqHeight){
+        final int height = options.outHeight;
+        final int width = options.outWidth;;
+        int inSampleSize = 1;
+
+        if(height > reqHeight || width > reqWidth){
+            final int halfHeight = height/2 ;
+            final int halfWidth = width/2 ;
+
+            while((halfHeight / inSampleSize) >= reqHeight && (halfWidth / inSampleSize) >= reqWidth){
+                inSampleSize *= 2;
+            }
+        }
+        return inSampleSize;
     }
 }
