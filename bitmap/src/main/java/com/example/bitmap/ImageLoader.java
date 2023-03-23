e26dc35071d6403fde1074abac105ab05aae6f79
yn030727 <2601098388@qq.com>
2023-03-23 15:55:36 +0800

    1. finish view

---
+++ bitmap/src/main/java/com/example/bitmap/ImageLoader.java
@@ -0,0 +1,11 @@
+package com.example.bitmap;
+
+import android.graphics.Bitmap;
+import android.util.LruCache;
+
+import java.util.BitSet;
+
+public class ImageLoader {
+    private LruCache<String , Bitmap> mMemoryCache;
+
+}
