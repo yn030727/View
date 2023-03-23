640da524541ec732fee0c5eab36e088cd894d1a7
yn030727 <2601098388@qq.com>
2023-03-23 15:01:51 +0800

    1. finish view

--- app/handler1/src/main/java/com/example/handler1/MainActivity.java
+++ app/handler1/src/main/java/com/example/handler1/MainActivity.java
@@ -3,19 +3,52 @@ package com.example.handler1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Looper;
 import android.util.Log;
+import android.util.LruCache;
+
+import com.jakewharton.disklrucache.DiskLruCache;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     private ThreadLocal<Boolean> mBooleanThreadLocal = new ThreadLocal<Boolean>();
+
+    private static final long DISK_CACHE_SIZE = 1024 * 1025 * 50;
+    File diskCacheDir = getDiskCacheDir(this , "bitmap");
+    DiskLruCache mDiskLruCache;
+    {
+        if(!diskCacheDir.exists()){
+            diskCacheDir.mkdirs();
+        }
+        try {
+            mDiskLruCache = DiskLruCache.open(diskCacheDir , 1 , 1 , DISK_CACHE_SIZE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public File getDiskCacheDir(Context context , String uniqueName){
+        String cachePath;
+        if(Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState()) || !Environment.isExternalStorageRemovable()){
+            cachePath = context.getExternalCacheDir().getPath();
+        }else{
+            cachePath = context.getCacheDir().getPath();
+        }
+        return new File(cachePath + File.separator + uniqueName);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +91,23 @@ public class MainActivity extends AppCompatActivity {
 //                Looper.loop();
 //            }
 //        }.start();
+
+
+//        Bitmap bitmap = new BitmapFactory();
+//
+//
+//        //总容量的大小为当前进程可用内存的1/8，单位为KB
+//        int maxMemory = (int)(Runtime.getRuntime().maxMemory() / 1024);
+//        int cacheSize = maxMemory / 8 ;
+//        LruCache mMemoryCache = new LruCache<String , Bitmap>(cacheSize){
+//            //sizeOf的作用是计算缓存对象的大小
+//            @Override
+//            protected int sizeOf(String key, Bitmap value) {
+//                //除以1024也是为了将其单位转化成KB(缓存的大小单位需要和总容量的单位一致）
+//                return bitmap.getRowBytes() * bitmap.getHeight() / 1024;
+//            }
+//        };
+
 
     }
 
