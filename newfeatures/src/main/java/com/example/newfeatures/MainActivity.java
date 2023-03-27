97d74cef6f6433ce0dc7794d786126451999f797
yn030727 <2601098388@qq.com>
2023-03-27 10:41:23 +0800

    1. finish view

--- newfeatures/src/main/java/com/example/newfeatures/MainActivity.java
+++ newfeatures/src/main/java/com/example/newfeatures/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.newfeatures;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
@@ -16,8 +17,13 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView mRecyclerView = (RecyclerView)findViewById(R.id.mRecyclerView);
 
 
+
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
         mRecyclerView.setLayoutManager(linearLayoutManager);
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this , DividerItemDecoration.VERTICAL_LIST));
+
+        mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(4 , StaggeredGridLayoutManager.VERTICAL));
+       // mRecyclerView.addItemDecoration(new DividerGridItemDecoration(this));
     }
 }
