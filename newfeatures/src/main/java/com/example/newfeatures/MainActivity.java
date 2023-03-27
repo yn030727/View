ed0c0bc8cb8eca99a6a60c70dfd321e409f375f6
yn030727 <2601098388@qq.com>
2023-03-27 10:00:28 +0800

    1. finish view

--- newfeatures/src/main/java/com/example/newfeatures/MainActivity.java
+++ newfeatures/src/main/java/com/example/newfeatures/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.newfeatures;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView mRecyclerView = (RecyclerView)findViewById(R.id.mRecyclerView);
+
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
     }
 }
