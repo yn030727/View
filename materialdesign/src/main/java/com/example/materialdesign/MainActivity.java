26d68d1c880aff49579396f10a57d41e031c4a6b
yn030727 <2601098388@qq.com>
2023-03-28 18:19:59 +0800

    1. finish view

---
+++ materialdesign/src/main/java/com/example/materialdesign/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.materialdesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
