9c442e50d057662f0fe223ac0b595be26917cfa8
yn030727 <2601098388@qq.com>
2023-04-07 00:25:34 +0800

    1. finish view

--- httpurlconnection/src/main/java/com/example/httpurlconnection/MainActivity.java
+++ httpurlconnection/src/main/java/com/example/httpurlconnection/MainActivity.java
@@ -2,9 +2,18 @@ package com.example.httpurlconnection;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.io.IOException;
 
@@ -14,7 +23,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        RequestQueue mQueue = Volley.newRequestQueue(getApplicationContext());
+        StringRequest mStringRequest = new StringRequest(Request.Method.GET, "http://192.168.1.117:8084/ping", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("Ning" , response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Ning" , error.getMessage() , error);
+            }
+        });
+
+        mQueue.add(mStringRequest);
 
         //        Button button = (Button) findViewById(R.id.button1);
 //        button.setOnClickListener(new View.OnClickListener() {
