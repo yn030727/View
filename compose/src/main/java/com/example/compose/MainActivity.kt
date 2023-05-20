ba9ad53dc3e03cd51ccfd20b5feca272a2f24c57
yn030727 <2601098388@qq.com>
2023-05-20 15:30:14 +0800

    4.Compose

--- compose/src/main/java/com/example/compose/MainActivity.kt
+++ compose/src/main/java/com/example/compose/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.example.compose
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -16,23 +18,20 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.compose.ui.theme.ViewTheme
+import com.example.compose.ui.theme.Message
+import com.example.compose.ui.theme.SampleData
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme() {
-                androidx.compose.material.Surface(modifier = Modifier.fillMaxSize()) {
-                    MessageCard(Message("Android" , "Jetpack Compose"))
-                }
+                Conversation(messages = SampleData.conversationSample)
             }
         }
     }
 
-    //将两个参数封装成一个Message
-    //author和body是主构造方法传入的两个参数
-    data class  Message(val author : String , val body : String)
+
 
 
     @Composable
@@ -46,7 +45,8 @@ class MainActivity : ComponentActivity() {
                 modifier = Modifier
                     .size(40.dp)
                     .clip(CircleShape)
-                    .border(1.5.dp , MaterialTheme.colors.secondary , CircleShape)
+                    .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+            
             )
             
             //在图片和垂直文本块之间添加8dp的空白距离
@@ -54,23 +54,53 @@ class MainActivity : ComponentActivity() {
             
             Column() {
                 Text(text = "Hello ${message.author}!" ,
-                    color = MaterialTheme.colors.secondaryVariant
+                    color = MaterialTheme.colors.secondaryVariant,
+                    style = MaterialTheme.typography.subtitle2
                 )
 
                 Spacer(modifier = Modifier.height(4.dp))
 
-                Text(text = "Hello ${message.body}!")
+                androidx.compose.material.Surface(shape = MaterialTheme.shapes.medium , elevation = 1.dp) {
+                    Text(text = "Hello ${message.body}!",
+                        style = MaterialTheme.typography.subtitle2,
+                        modifier = Modifier.padding(all = 4.dp)
+                    )
+                }
             }
         }
     }
 
-    @Preview(showBackground = true)
+
+    @Preview(name = "Light Mode")
+    @Preview(
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        showBackground = true,
+        name = "Dark Mode"
+    )
     @Composable
     fun previewMessageCard(){
         MaterialTheme() {
             androidx.compose.material.Surface() {
                 MessageCard(message = Message("Colleague" , "Hey , take a look at Jetpack Compose !"))
             }
+        }
+    }
+    
+    
+    @Composable
+    fun Conversation(messages: List<com.example.compose.ui.theme.Message>){
+        LazyColumn{
+            items(messages){ message ->
+                MessageCard(message = message)
+            }
+        }
+    }
+    
+    @Preview
+    @Composable
+    fun PreviewConversation(){
+        MaterialTheme(){
+            Conversation(messages = SampleData.conversationSample)
         }
     }
     
---
+++ compose/src/main/java/com/example/compose/ui/theme/Message.kt
@@ -0,0 +1,3 @@
+package com.example.compose.ui.theme
+
+data class Message(val author : String , val body : String)
