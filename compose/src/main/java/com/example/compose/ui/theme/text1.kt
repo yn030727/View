ef163254df93036be6f79bff121ed7b4124d07d7
yn030727 <2601098388@qq.com>
2023-05-28 11:19:04 +0800

    4.Compose

--- compose/src/main/java/com/example/compose/ui/theme/text1.kt
+++ compose/src/main/java/com/example/compose/ui/theme/text1.kt
@@ -24,3 +24,37 @@ fun main(){
         }
     }
 }
+
+fun strToInt(str : String) : Int{
+    val length = str.length
+    var flag = 0
+    var number = 0
+    var temp = 1
+    for (i in 0 until length){
+        if(str.get(i) != ' '){
+            if(flag == 0 && !(str.get(i) <= '9' && str.get(i) >= '0')){
+                if(str.get(i) == '-'){
+                    temp = -1
+                }else {
+                    return 0
+                }
+            }else if(str.get(i) <= '9' && str.get(i) >= '0'){
+                if(flag == 0){
+                    number = str.get(i) - '0'
+                }else{
+                    number = number * 10 + (str.get(i) - '0')
+                }
+                flag++
+            }else{
+                return number
+            }
+        }
+        println(number)
+    }
+    if(number * temp > Int.MAX_VALUE){
+        return Int.MAX_VALUE
+    }else if(number * temp < Int.MIN_VALUE){
+        return Int.MIN_VALUE
+    }
+    return number * temp
+}
