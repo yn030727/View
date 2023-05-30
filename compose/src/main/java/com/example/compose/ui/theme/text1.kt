b559ff1a3e35311baf4f1182115e945904acd858
yn030727 <2601098388@qq.com>
2023-05-30 19:59:39 +0800

    4.Compose

--- compose/src/main/java/com/example/compose/ui/theme/text1.kt
+++ compose/src/main/java/com/example/compose/ui/theme/text1.kt
@@ -28,36 +28,43 @@ fun main(){
     println(data1.toString())
 }
 
-fun strToInt(str : String) : Int{
-    val length = str.length
-    var flag = 0
-    var number = 0
-    var temp = 1
-    for (i in 0 until length){
-        if(str.get(i) != ' '){
-            if(flag == 0 && !(str.get(i) <= '9' && str.get(i) >= '0')){
-                if(str.get(i) == '-'){
-                    temp = -1
-                }else {
-                    return 0
-                }
-            }else if(str.get(i) <= '9' && str.get(i) >= '0'){
-                if(flag == 0){
-                    number = str.get(i) - '0'
-                }else{
-                    number = number * 10 + (str.get(i) - '0')
-                }
-                flag++
-            }else{
-                return number
-            }
-        }
-        println(number)
-    }
-    if(number * temp > Int.MAX_VALUE){
-        return Int.MAX_VALUE
-    }else if(number * temp < Int.MIN_VALUE){
-        return Int.MIN_VALUE
-    }
-    return number * temp
+//fun strToInt(str : String) : Int{
+//    val length = str.length
+//    var flag = 0
+//    var number = 0
+//    var temp = 1
+//    for (i in 0 until length){
+//        if(str.get(i) != ' '){
+//            if(flag == 0 && !(str.get(i) <= '9' && str.get(i) >= '0')){
+//                if(str.get(i) == '-'){
+//                    temp = -1
+//                }else {
+//                    return 0
+//                }
+//            }else if(str.get(i) <= '9' && str.get(i) >= '0'){
+//                if(flag == 0){
+//                    number = str.get(i) - '0'
+//                }else{
+//                    number = number * 10 + (str.get(i) - '0')
+//                }
+//                flag++
+//            }else{
+//                return number
+//            }
+//        }
+//        println(number)
+//    }
+//    if(number * temp > Int.MAX_VALUE){
+//        return Int.MAX_VALUE
+//    }else if(number * temp < Int.MIN_VALUE){
+//        return Int.MIN_VALUE
+//    }
+//    return number * temp
+//}
+
+
+fun reversePrint(head : List<Int?>) : IntArray {
+    val array = IntArray(5)
+    array.set(0 ,1)
+    return array
 }
