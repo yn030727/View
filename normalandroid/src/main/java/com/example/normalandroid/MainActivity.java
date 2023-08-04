6d4096c2932fabc94f34fe8c57b9ee73c5b4d9d1
yn030727 <2601098388@qq.com>
2023-08-05 03:06:47 +0800

    4.Compose

--- normalandroid/src/main/java/com/example/normalandroid/MainActivity.java
+++ normalandroid/src/main/java/com/example/normalandroid/MainActivity.java
@@ -5,31 +5,49 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import java.net.URI;
 
+@SuppressLint("MissingInflatedId")
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RightFragment rightFragment = new RightFragment();
-        LeftFragment leftFragment = new LeftFragment();
 
+        TextView textView = findViewById(R.id.text1111);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Ning" , "onClick");
 
-        Uri uri = Uri.parse("app://hello");
-        Intent intent = new Intent(Intent.ACTION_VIEW , uri);
+                NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                    NotificationChannel channel = new NotificationChannel("1" , "重要信息" , NotificationManager.IMPORTANCE_HIGH);
+                    manager.createNotificationChannel(channel);
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.layout_fragment , leftFragment);
-        fragmentTransaction.commit();
-
-        Bundle bundle = new Bundle();
-        bundle.putString("niubi" , "123");
-        leftFragment.setArguments(bundle);
+                    Notification notification = new Notification.Builder(MainActivity.this , "1")
+                            .setContentTitle("This is content title")
+                            .setContentText("This is content text")
+                            .setWhen(System.currentTimeMillis())
+                            .setSmallIcon(R.mipmap.ic_launcher)
+                            .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
+                            .build();
+                    manager.notify(1 , notification);
+                }
+            }
+        });
     }
 }
